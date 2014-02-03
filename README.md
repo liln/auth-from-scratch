bef9d90b230f7e8912bec0a5c64f773bf05938c1
Lillian Ng <lillian.k.ng@gmail.com>
2014-02-02 21:32:23 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Authorization for a list
+
+## Description
+This application uses Rails v3.2.16 and follows along with RailsCast#250 for authorization from scratch. The result is a list where anyone can add to it, but only logged in users can edit, update, and destroy.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Auth::Application.config.secret_token = '580f3ed5cf1cbeeafd9ef0f0fd905196ebc90c01554424236a7a8fe87e1cfce7dc352a44e677b3205411a3efec6f96e1dd7b2248ba40827789665877002dd0c5'
